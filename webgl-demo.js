be2125fafc272912a8fb65c78718aebc255a3633
JacobersCom <jacobymartin1@icloud.com>
2026-01-11 20:53:38 -0500

    Init shaderprogram

--- webgl-demo.js
+++ webgl-demo.js
@@ -19,3 +19,32 @@ function main()
 // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
+
+function InitializeShaderProgram(gl, vsSource, psSource)
+{
+    const VertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const PixelShader = loadShader(gl, gl.VERTEX_SHADER, psSource);
+
+    //Create shader program
+
+    const ShaderProgram = gl.createProgram();
+    gl.attachShader(ShaderProgram, VertexShader);
+    gl.attachShader(ShaderProgram, PixelShader);
+    gl.linkProgram(ShaderProgram);
+
+    //If creating program filed
+
+    if (!gl.getProgramParameter(ShaderProgram, gl.LINK_STATUS)) 
+    {
+        //The alert uses string interpolations allowing vars to be placed 
+        //right in the string
+        alert(
+            `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+            ShaderProgram,
+        )}`,
+    );
+    return null;
+  }
+
+  return ShaderProgram;
+}
